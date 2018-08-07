9e9d3e5c2453b865a8ad877a4b397d236a39c639
Vinicius.saraiva Acc <vinicius.saraiva.acc@accurate.com.br>
2018-08-07 00:29:24 -0300

    make it better

--- src/test/java/br/com/saraiva/OptionalTest.java
+++ src/test/java/br/com/saraiva/OptionalTest.java
@@ -10,21 +10,28 @@ public class OptionalTest {
 
     @Test
     public void testMap() {
-        final Optional<String> s = Optional.of("test");
-        assertEquals(Optional.of("TEST"), s.map(String::toUpperCase));
+
+        final Optional<String> expected = Optional.of("TEST");
+        final Optional<String> actual = Optional.of("test").map(String::toUpperCase);
+
+         assertEquals(expected, actual);
     }
 
     @Test
     public void testMapNotFlat() {
-        assertEquals(Optional.of(Optional.of("STRING")),
-                Optional.of("string")
-                        .map(s -> Optional.of("STRING")));
+
+        final Optional<Optional<String>> expected = Optional.of(Optional.of("STRING"));
+        final Optional<Optional<String>> actual = Optional.of("string").map(s -> Optional.of("STRING"));
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testFlatMap() {
-        assertEquals(Optional.of("STRING"), Optional
-                .of("string")
-                .flatMap(s -> Optional.of("STRING")));
+
+        final Optional<String> expected = Optional.of("STRING");
+        final Optional<String> actual = Optional.of("string").flatMap(s -> Optional.of("STRING"));
+
+        assertEquals(expected, actual);
     }
 }
